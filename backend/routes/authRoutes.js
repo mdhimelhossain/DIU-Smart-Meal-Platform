75a0ab0ffebad77da6e89c7c7a83487f25135b67
alimuhammadsagor <sagor.stu4@gmail.com>
2026-04-11 15:46:04 +0600

    added backend\server.js

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { register, login, me } = require("../controllers/authController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-router.post("/register", register);
-router.post("/login", login);
-router.get("/me", authMiddleware, me);
-
-module.exports = router;
